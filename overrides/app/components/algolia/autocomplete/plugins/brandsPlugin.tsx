a57c09f2d1f4ecd9041f63fb90f902c7129e3945
Sylvain Bellone <sylvain.bellone@algolia.com>
2024-10-07 15:48:18 +0200

    fix: don't require Recommend for brands

--- overrides/app/components/algolia/autocomplete/plugins/brandsPlugin.tsx
+++ overrides/app/components/algolia/autocomplete/plugins/brandsPlugin.tsx
@@ -1,11 +1,11 @@
 /** @jsxRuntime classic */
 /** @jsx React.createElement */
-import {AutocompletePlugin, getAlgoliaFacets} from '@algolia/autocomplete-js'
+import {AutocompletePlugin, getAlgoliaResults} from '@algolia/autocomplete-js'
 import React, {createElement, Fragment} from 'react'
 import {ALGOLIA_PRODUCTS_INDEX_NAME} from '../constants'
 import {BrandHit} from './../types'
 import {BrandItem} from './../components/BrandItem'
-import {recommendClient} from '../recommendClient'
+import {searchClient} from '../searchClient'
 
 /**
  * An Autocomplete Plugin that provides brand results from Algolia.
@@ -20,14 +20,45 @@ export const brandsPlugin: AutocompletePlugin<BrandHit, {}> = {
             {
                 sourceId: 'brandsPlugin',
                 async getItems() {
-                    var response = await recommendClient.getTrendingFacets([
-                        {
-                          indexName: ALGOLIA_PRODUCTS_INDEX_NAME,
-                          facetName: 'brand'
+                    return getAlgoliaResults({
+                        searchClient,
+                        queries: [
+                            {
+                                indexName: ALGOLIA_PRODUCTS_INDEX_NAME,
+                                query: '',
+                                params: {
+                                    facets: ['brand'],
+                                    hitsPerPage: 1,
+                                }
+                            }
+                        ],
+                        transformResponse({ results }) {
+                            console.log(results)
+                            const brandFacets = results[0].facets['brand'];
+                            if (!brandFacets) {
+                                return [];
+                            }
+                            const res = [];
+                            for (let [key, value] of Object.entries(brandFacets)) {
+                                res.push({ facetValue: key, count: value });
+                            }
+                            // Sort category facets by their count and return only the first 4
+                            res.sort((a, b) => {
+                                return b.count - a.count;
+                            });
+                            return [res.slice(0, 4)];
                         },
-                    ])
-
-                    return response.results[0].hits
+                    })
+                    // Once you have Recommend setup, you may want to use it to display the
+                    // trending brands. This can be achieved like that:
+                    // var response = await recommendClient.getTrendingFacets([
+                    //     {
+                    //       indexName: ALGOLIA_PRODUCTS_INDEX_NAME,
+                    //       facetName: 'brand',
+                    //     },
+                    // ])
+                    //
+                    // return response.results[0].hits
                 },
                 renderer: {createElement, Fragment, render: () => {}},
                 templates: {
@@ -41,4 +72,3 @@ export const brandsPlugin: AutocompletePlugin<BrandHit, {}> = {
         ]
     }
 }
-
